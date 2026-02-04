2ebc526c27448c89a35ba7d143bcf9c35c09ba83
Maria Luisa Santiago Vieira <marialuisasantiagoccp@gmail.com>
2026-02-03 21:50:42 -0300

    Add Footer

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -10,6 +10,7 @@ import Contato from './pages/Contato/Contato.jsx'
 
 //Componentes
 import Navbar from './components/Navbar/Navbar.jsx'
+import Footer from './components/Footer/Footer.jsx'
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path='/contato' element={<Contato></Contato>}></Route>
         
       </Routes>
+    <Footer/>
      
     </BrowserRouter>
   
